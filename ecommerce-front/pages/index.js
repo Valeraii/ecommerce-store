cfc0563e1c75177ab3a0015dae7f45e4a28e004e
valerie <valerietan000@gmail.com>
2023-06-11 11:11:35 -0700

    added featured products to store

---
+++ ecommerce-front/pages/index.js
@@ -0,0 +1,22 @@
+import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
+
+export default function HomePage({product}) {
+  return (
+    <div>
+      <Header />
+      <Featured product={product}/>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "6480fa8bb74e611268241e39";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: {product: JSON.parse(JSON.stringify(product))}
+  }
+}
